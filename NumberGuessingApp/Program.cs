42ac4e848d715c082c10d3f2bb37496c7a3bdcd7
badassmessiah <lashaandguladze69@gmail.com>
2024-02-16 13:27:02 +0400

    Remove Input Validation Class, Implement Game Logic

--- NumberGuessingApp/Program.cs
+++ NumberGuessingApp/Program.cs
@@ -27,7 +27,43 @@
             int min = 1;
             int max = DifficultyChooser.ChooseDifficulty();
 
+
+            int number = new Random().Next(min, max);
+            int attempts = 10;
+
             
+            while (attempts > 0)
+            {
+                Console.WriteLine($"Please enter your guess (1-{max}): ");
+                string input = Console.ReadLine();
+                int guess;
+
+                if (int.TryParse(input, out guess))
+                {
+                    if (guess == number)
+                    {
+                        Console.WriteLine($"Congratulations, {name}! You have guessed the number.");
+                        score = attempts;
+                        break;
+                    }
+                    else if (guess > number)
+                    {
+                        Console.WriteLine("The number is lower.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is higher.");
+                    }
+
+                    attempts--;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+
+                
+            }
 
 
 
