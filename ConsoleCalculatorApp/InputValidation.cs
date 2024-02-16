596c73d6eb9f0f25d047d26e529c6f90858bc415
badassmessiah <lashaandguladze69@gmail.com>
2024-02-16 12:26:34 +0400

    Move Validations to Validation class

--- ConsoleCalculatorApp/InputValidation.cs
+++ ConsoleCalculatorApp/InputValidation.cs
@@ -4,12 +4,37 @@
     {
         public static double ValidateNumber(string input)
         {
-            if (!double.TryParse(input, out double number))
+            if (string.IsNullOrWhiteSpace(input) || input.Trim() != input || !double.TryParse(input, out double number))
             {
                 Console.WriteLine("Invalid input. Please enter a valid number.");
                 return double.NaN;
             }
             return number;
+        }
+
+        public static double GetValidNumber(string userInput)
+        {
+            double number;
+            do
+            {
+                Console.WriteLine(userInput);
+                string input = Console.ReadLine();
+                number = InputValidation.ValidateNumber(input);
+            } while (double.IsNaN(number));
+
+            return number;
+        }
+
+        public static string GetValidOperation(string userInput)
+        {
+            string operation;
+            do
+            {
+                Console.WriteLine(userInput);
+                operation = Console.ReadLine();
+            } while (operation == null || !"asmd".Contains(operation.ToLower()));
+
+            return operation;
         }
     }
 }
--- ConsoleCalculatorApp/Program.cs
+++ ConsoleCalculatorApp/Program.cs
@@ -8,9 +8,9 @@
 
             while (true)
             {
-                double firstNumber = GetValidNumber("\nEnter First Number");
-                double secondNumber = GetValidNumber("Enter Second Number");
-                string operation = GetValidOperation("Enter Operation. A for Add, S for Subtract, M for Multiply, D for Divide");
+                double firstNumber = InputValidation.GetValidNumber("\nEnter First Number");
+                double secondNumber = InputValidation.GetValidNumber("Enter Second Number");
+                string operation = InputValidation.GetValidOperation("Enter Operation. A for Add, S for Subtract, M for Multiply, D for Divide");
 
                 switch (operation.ToLower())
                 {
@@ -30,29 +30,6 @@
             }
         }
 
-        static double GetValidNumber(string userInput)
-        {
-            double number;
-            do
-            {
-                Console.WriteLine(userInput);
-                string input = Console.ReadLine();
-                number = InputValidation.ValidateNumber(input);
-            } while (double.IsNaN(number));
-
-            return number;
-        }
-
-        static string GetValidOperation(string userInput)
-        {
-            string operation;
-            do
-            {
-                Console.WriteLine(userInput);
-                operation = Console.ReadLine();
-            } while (operation == null || !"asmd".Contains(operation.ToLower()));
-
-            return operation;
-        }
+       
     }
 }
