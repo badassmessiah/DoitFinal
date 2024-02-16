0011c105f7803af423f71ade3bc5162ee882ab7c
badassmessiah <lashaandguladze69@gmail.com>
2024-02-16 13:13:27 +0400

    Optimize Difficulty Chooser

--- NumberGuessingApp/DifficultyChooser.cs
+++ NumberGuessingApp/DifficultyChooser.cs
@@ -4,35 +4,33 @@ public class DifficultyChooser
 {
     public static int ChooseDifficulty()
     {
-        string difficulty;
         int max = 0;
+        string input;
 
         do
         {
             Console.WriteLine("Choose difficulty: Easy (e), Medium (m), Hard (h)");
-            difficulty = InputValidation.ValidateDifficulty(Console.ReadLine().ToLower());
+            input = Console.ReadLine().ToLower();
 
-            switch (difficulty)
+            switch (input)
             {
                 case "e":
-                    Console.WriteLine("You have chosen Easy difficulty. You have 10 attempts to guess the number between 1 and 15.");
                     max = 15;
                     break;
                 case "m":
-                    Console.WriteLine("You have chosen Medium difficulty. You have 10 attempts to guess the number between 1 and 25.");
                     max = 25;
                     break;
                 case "h":
-                    Console.WriteLine("You have chosen Hard difficulty. You have 10 attempts to guess the number between 1 and 50.");
                     max = 50;
                     break;
                 default:
                     Console.WriteLine("Invalid input. Please choose difficulty again.");
-                    difficulty = null;
+                    max = 0;
                     break;
             }
-        } while (difficulty == null);
+        } while (max == 0);
 
+        Console.WriteLine($"You have chosen {input.ToUpper()} difficulty. You have 10 attempts to guess the number between 1 and {max}.");
         return max;
     }
 }
